af896ddba88d1d768ce74f06fded19649720640a
arroe99 <113248688+arroe99@users.noreply.github.com>
2022-09-13 22:11:31 +0800

    Add files via upload

---
+++ wallhaven壁纸爬虫try.R
@@ -0,0 +1,31 @@
+rm(list = ls())  #删除所有变量
+getwd()          #查看R语言文件的保存和读取的路径
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))  #设置路径为当前R脚本所在路径
+
+library(rvest)
+library(downloader)
+library(tidyverse)
+
+filename <- "wallhaven爬虫"
+if(! dir.exists(filename)){
+  dir.create(filename)
+}
+
+
+url <- 'https://wallhaven.cc/search?categories=010&purity=100&resolutions=1920x1080&topRange=1y&sorting=toplist&order=desc&page=1'
+#图片详情页网址
+picture <- read_html(url) %>%
+           html_nodes("a.preview") %>%
+           html_attr("href") %>% print()
+
+
+for(i in 1:length(picture)){
+  print(picture[i])
+  download_url <- read_html(picture[i]) %>%
+      html_nodes('#wallpaper') %>%
+      html_attr("src") %>% print()
+  download(download_url,paste(filename,"/",i,str_sub(download_url,-4,-1),sep = ""), mode = "wb")
+  Sys.sleep(2)
+} 
+
+
